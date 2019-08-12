a97ad8cf55c2ef8e9453119e99439ebce87475b9
picktunes <53955902+picktunes@users.noreply.github.com>
2019-08-11 23:50:56 -0300

    Update wmaPlay.java

--- Etapa1/wmaPlay.java
+++ Etapa1/wmaPlay.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package trabalhounidade02;
+package trabalhounidade2;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -14,32 +14,51 @@ import javax.swing.JFrame;
  */
 public class wmaPlay {
     
+    private String arquivoSetado;
+    private Estado arquivoEstado;
+    private int arquivoPosicao;
     
     public void setFile(String s){
-    final JFrame parent = new JFrame();
-        JButton button = new JButton();
-        
+    this.arquivoSetado = s;
+    this.arquivoEstado = Estado.SETADO;
     }
     
     public void setLocation(int i){
-        
+   if (i > 0){
+       this.arquivoPosicao = i;
+       this.arquivoEstado = Estado.PARADO;
+       } else
+       this.arquivoEstado = Estado.PARADO;
+       this.arquivoPosicao = 0;
     }
     
     public int getLocation(){
-        
-    return 0;    
+    return this.arquivoPosicao;    
     }
     
-    public void play(){
-        
+    public void reproduzir(){
+        if (this.arquivoEstado == Estado.PAUSADO || this.arquivoEstado == Estado.SETADO || this.arquivoEstado == Estado.PARADO ){
+        this.arquivoEstado = Estado.REPRODUZINDO;
+        System.out.println("O arquivo " + this.arquivoSetado.toString() + " foi executado a partir dos " + this.arquivoPosicao + " milisegundos.");
+        }
     }
     
-    public void stop(){
-        
+    public void parar(){
+    
+        if(this.arquivoEstado == Estado.REPRODUZINDO){
+            this.arquivoEstado = Estado.PAUSADO;
+           System.out.println("O arquivo foi pausado aos " + this.arquivoPosicao + " milisegundos.");
+        }else{
+            
+            this.arquivoEstado = Estado.PAUSADO;
+            this.setLocation(0);
+            System.out.println("O tempo de reprodução voltou ao início.");
+        }
     }
     
-    public void open(){
-        
+     public void abrir() {
+        if (this.arquivoEstado == Estado.SETADO) {
+            this.arquivoEstado = Estado.ABERTO;
+        }
     }
-    
 }
